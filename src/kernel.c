976242af8521627a33e8b8faa95458c0a2574b7a
sharmavins23 <sharmavins23@gmail.com>
2022-01-02 21:55:02 -0500

    updated uart_send_string with printf formatting

--- src/kernel.c
+++ src/kernel.c
@@ -1,17 +1,30 @@
 #include "common.h"
 #include "mini_uart.h"
+#include "printf.h"
+
+// Printf initialization function for placing a simple character
+void putc(void *p, char c) {
+    if (c == '\n')
+        putc(p, '\r');
+
+    uart_send(c);
+}
 
 void kernel_main() {
     uart_init();
-    uart_send_string("Raspberry Pi Bare Metal OS Initializing...\n");
+
+    // Initialize printf's formatting system with our putc function for UART
+    init_printf(0, putc);
+
+    printf("Raspberry Pi Bare Metal OS Initializing...\n");
 
 #if RPI_VERSION == 3
-    uart_send_string("\tRaspberry Pi 3 \n");
+    printf("\tRaspberry Pi 3 \n");
 #elif RPI_VERSION == 4
-    uart_send_string("\tRaspberry Pi 4 \n");
+    printf("\tRaspberry Pi 4 \n");
 #endif
 
-    uart_send_string("\n\nDone!\n");
+    printf("\n\nDone!\n");
 
     while (1) {
         uart_send(uart_recv());
